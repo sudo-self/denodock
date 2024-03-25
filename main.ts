dca2fcab1048aca693d89bcf2973fcd0fe371b95
Sudo-Self <sudo-self@proton.me>
2024-03-25 06:35:12 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -40,7 +40,9 @@ async function handleRequest(request) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deno HTML JSON</title>
+    <title>denodock</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
 
